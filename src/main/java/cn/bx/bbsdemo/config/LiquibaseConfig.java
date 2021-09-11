e86f1c2dd6041853ee2a6f87604346277941a88d
likai <327381633@qq.com>
2021-09-11 15:19:10 +0800

    测试一下提交

--- src/main/java/cn/bx/bbsdemo/config/LiquibaseConfig.java
+++ src/main/java/cn/bx/bbsdemo/config/LiquibaseConfig.java
@@ -1,35 +1,35 @@
-//package cn.bx.bbsdemo.config;
-//
-//import liquibase.integration.spring.SpringLiquibase;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.sql.DataSource;
-//
-///**
-// *
-// * Liquibase 的配置文件
-// */
-//@Configuration
-//public class LiquibaseConfig {
-//
-//    @Qualifier("dataSource")
-//    private DataSource dataSource;
-//
-//    @Bean
-//    public SpringLiquibase liquibase() {
-//        SpringLiquibase liquibase = new SpringLiquibase();
-//        liquibase.setDataSource(dataSource);
-//        liquibase.setChangeLog("classpath:liquibase/changelog/master.xml");
-//        liquibase.setContexts("development,test,preproduction,production");
-//        liquibase.setShouldRun(true);
-//        return liquibase;
-//    }
-//
-//    @Autowired
-//    public void setDataSource(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
-//}
+package cn.bx.bbsdemo.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ *
+ * Liquibase 的配置文件
+ */
+@Configuration
+public class LiquibaseConfig {
+
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:liquibase/changelog/master.xml");
+        liquibase.setContexts("development,test,preproduction,production");
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+}
